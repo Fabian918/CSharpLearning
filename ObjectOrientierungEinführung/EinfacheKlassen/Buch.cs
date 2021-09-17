2159901a28e656cb0e01e94ab1315cf0d32443d5
Fabian Hering <Fabian.Hering@plan-b-gmbh.com>
2021-09-17 12:11:52 +0200

    Add project files.

---
+++ ObjectOrientierungEinführung/EinfacheKlassen/Buch.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientierungEinführung.EinfacheKlassen
+{
+    public class Buch
+    {
+        /*
+         *  Variablen ,
+         *  sollten nur Private deklariert werden ( also nur intern, in der Klasse zugreifbar) 
+         * führen keinen Code aus, zeigen direkt auf den Wert im Speicher 
+         */
+
+        private int seitenAnzahl;
+
+        private string text;
+
+        private bool istEinKrimi;
+
+
+        /* 
+         * Properties,
+         * können Code ausführen, und müssen nicht direkt auf einen Speicher zeigen. 
+         * Wichtig ist: sie verhalten sich für den Aufrufer WIE eine Variable. 
+         * Intern müssen wir mit get & set dafür sorgen dass sie das auch tun.
+         *  
+         *  GET -> ist der lesende Zugriff.
+         *  SET -> ist der schreibende Zugriff (gibt eine value vom typ der Property mit)
+         * 
+         */
+
+        public int SeitenAnzahl
+        {
+            get
+            {
+                return this.seitenAnzahl;
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return this.text;
+            }
+            set
+            {
+                this.text = value;
+            }
+        }
+
+        public bool IstEinKrimi
+        {
+            get
+            {
+                return this.istEinKrimi;
+            }
+        }
+
+        
+        /* 
+         * Konstruktoren
+         *  --> über Konstruktoren werden Instancen der Klasse erstellt. 
+         *  wenn kein Konstruktor definiert wird, gibt uns C# einen leeren Konstruktor vor.
+         * Wir können so kontrollieren, wie Instancen unserer Klassen erstellt werden
+         *   -> welche Parameter (Variablen) brauchen wir auf jeden Fall, welche sind optional
+         *   -> welche Dinge müssen direkt beim erstellen erledigt werden
+         */
+
+        public Buch()
+        {
+            this.seitenAnzahl = 0;
+            this.text = "";
+            this.istEinKrimi = false;
+        }
+
+        public Buch(int seitenAnzahl, string text)
+        {
+            this.seitenAnzahl = seitenAnzahl;
+            this.text = text;
+            this.istEinKrimi = false;
+        }
+
+        public Buch(int seitenAnzahl, string text, bool istEinKrimi)
+        {
+            this.seitenAnzahl = seitenAnzahl;
+            this.text = text;
+            this.istEinKrimi = istEinKrimi;
+        }
+
+        /*
+         * Methoden,
+         * können Private oder Public sein
+         *  --> Methoden können 
+         *      --> void zurückgeben (Keinen Rückgabewert) 
+         *      --> Belibige Datentypen zurückgeben (string, int , bool, ...) 
+         * --> Methoden füren aktiv Code aus, wenn sie aufgerufen werden.
+         * --> Methoden arbeiten mit ihren Übergabeparamter ( -> von extern) und den Instanzvariablen ( -> Klassen intern)  
+         */ 
+
+        public void FügeTextHinzu(string textZumHinzufügen)
+        {
+            this.text = this.text + textZumHinzufügen;
+        }
+
+        public void LöscheText(int anfang, int länge)
+        {
+            this.text.Remove(anfang, länge);
+        }
+
+
+        
+        /*
+         * Hier würde sich auch eine Property eignen.
+         * Der RückgabeWert berechnet sich komplett aus Variablen.
+         * Die Methode verändert keine Instanzvariablen. 
+         * 
+         * Property oder Methdode: 
+         * die Property MUSS sich wie eine Variable verhalten, d.h: 
+         *  - keine Langen Opertation (Zeit) 
+         *  - keine Operationen die die Klasse mehr als im Erwarteten Maße beeinflussen
+         * 
+         * Im Allgemeine gilt: 
+         * wenn Komplex, groß --> Methode, 
+         * wenn nicht und komplett aus internen Variablen konstruirbar --> Property
+         */
+
+        public bool IstLängerAls(Buch buchZumVergleichen)
+        {
+            if(buchZumVergleichen.SeitenAnzahl > this.SeitenAnzahl)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Buch mit {this.seitenAnzahl} Seiten und folgendem Text : \'{this.text}\'. Es ist {(this.istEinKrimi ? "ein" : "kein")} Krimi";
+        }
+
+
+
+    }
+}
---
+++ ObjectOrientierungEinführung/Program.cs
@@ -0,0 +1,77 @@
+﻿using ObjectOrientierungEinführung.EinfacheKlassen;
+using System;
+
+namespace ObjectOrientierungEinführung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Buch buch = new Buch(20,"Mein Text");
+
+            Console.WriteLine(buch.ToString());
+
+            buch.Text = "Mein Buch";
+            //buch.SeitenAnzahl = 20; --> geht nicht, weil kein Set definiert wurde.
+            //buch.IstEinKrimi = true; --> geht nicht, weil kein Set definiert wurde.
+
+            Console.WriteLine(buch.ToString());
+
+            buch.FügeTextHinzu(" Hallo Welt");
+
+            Console.WriteLine(buch.ToString());
+
+            buch.LöscheText(buch.Text.Length - 4, 4);
+
+            Console.WriteLine(buch.ToString());
+
+
+            Buch buchzumVergleichen = new Buch(100,"Mein zweiter Text");
+
+            if(buchzumVergleichen.IstLängerAls(buch))
+            {
+                Console.WriteLine($"Buch {buchzumVergleichen} ist größer als {buch}");
+            }
+            else
+            {
+                Console.WriteLine($"Buch {buchzumVergleichen} ist kleiner oder gleich groß wie {buch}");
+            }
+
+            Konstructoren();
+
+            InstanzierungUndRefernzierung();
+
+        }
+
+
+        public static void Konstructoren()
+        {
+            Buch buch1 = new Buch(); // Leerer Konstruktor.
+            Buch buch2 = new Buch(20, "Buch2 Text"); // Leerer Konstruktor.
+            Buch buch3 = new Buch(50, "Buch3 Text", true); // Leerer Konstruktor.
+
+        }
+        public static void InstanzierungUndRefernzierung()
+        {
+
+            Buch buch1 = new Buch(20, "Das ist Buch 1", false); // verweist auf  Instanz 1
+            
+
+
+            Buch buch2 = new Buch(50, "Das ist Buch 2", true); // verweist auf Instaz 2
+
+            Console.WriteLine($"Augabe buch1: {buch1.ToString()}");
+            Console.WriteLine($"Augabe buch2: {buch2.ToString()}");
+            
+            
+            Buch buch3 = buch1; // verweist auf Instanz 1 
+
+            Console.WriteLine($"Augabe buch3: {buch3.ToString()}");
+
+            buch3 = new Buch(80, "Das ist Buch 3", false);
+
+            Console.WriteLine($"Augabe buch3: {buch3.ToString()}");
+        }
+    }
+}
